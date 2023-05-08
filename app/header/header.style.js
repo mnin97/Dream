450d2023d1930684f30c1cd099d995255affa7ac
mnin97 <111506962+mnin97@users.noreply.github.com>
2023-05-08 21:54:22 +0900

    styled-coponent

---
+++ app/header/header.style.js
@@ -0,0 +1,15 @@
+"use client";
+
+import { styled } from "styled-components";
+
+export const HeaderWrapper = styled.div`
+  box-shadow: 0 1px 0 0 rgba(0, 0, 0, 0.1);
+  width: 100%;
+  height: 130px;
+  background-color: #fff;
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  z-index: 1000;
+`;
---
+++ app/header/page.js
@@ -0,0 +1,5 @@
+import { HeaderWrapper } from "./header.style";
+
+export default function Header() {
+  return <HeaderWrapper></HeaderWrapper>;
+}
